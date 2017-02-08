b0bae99f0c1c6ce79c24ed63480c4eccd1526f78
Diego Porras <andree.rivas@galileo.edu>
2017-02-08 09:56:33 -0600

    Ejercicio 1.2 con punto flotante

--- fahr.c
+++ fahr.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main(void){
-	/*Imprime los grados fahrenheit de 0 a 300 y su equivalente en celcius*/
-	int low=0;
-	int upper=300;
-	int step=20;
-	int fahr=low;
+	/*Imprime los grados fahrenheit de 0 a 300 y su equivalente en celcius;
+	*Con punto flotante para mayor precicion
+	*/
+	float low,upper,step;
+	float fahr,celcius;
+	low=0;
+	upper=300;
+	step=20;
+	fahr=low;
 	while(fahr<=upper){
-		int celcius = 5 * (fahr - 32)/9;
-		printf("%d\t%d\n",fahr,celcius);
+		celcius = (5.0/9.0) * (fahr - 32.0);
+		printf("%3.0f\t%6.1f\n",fahr,celcius);
 		fahr+=step;
 	}
 	return 1;
